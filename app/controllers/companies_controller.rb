77de1a07d6d2e6b559989badb8fd6129ff7273dc
samira <s.soltani336@gmail.com>
2017-04-05 00:00:12 +0800

    Allow  creating new companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,31 @@
+class CompaniesController < ApplicationController
+
+	def index
+		@companies = Company.all	
+	end
+	
+
+	def new
+		@company = Company.new
+	end
+
+	def create
+		@company = Company.new(company_params)
+		if @company.save
+			redirect_to @company, notice: "the company   \"#{@company.name}\"  was built successfully"
+		else
+			render "new"
+		end
+	end
+
+	def show
+		@company= Company.find(params[:id])
+	end
+
+	private
+
+	def company_params
+		params.require(:company).permit(:name, :address, :directors, :logo_url, :custom_fields)
+	end
+
+end
