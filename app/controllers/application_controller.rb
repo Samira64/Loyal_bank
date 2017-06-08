0af426e044dd61619e37ba5d01d2fa21e8025bcc
Saarang <sahandfx@gmail.com>
2017-06-08 13:46:29 +0800

    Improve Sign In experience

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-  	if session[:user_id]
+    if session[:user_id]
   		@current_user ||= User.find(session[:user_id])
-  	end
+  	else
+      @current_user ||= nil
+    end
   end
+
   helper_method :current_user
 
   def authenticate_user
   	redirect_to login_path unless current_user
   end
-  
+
 end
